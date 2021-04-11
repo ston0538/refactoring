cc907e7b82fa6cddbcd91d3fedceb180cdeb3a8a
KHK <ston0538@gmail.com>
2021-04-11 23:32:57 +0900

    play 변수 제거하기
    [notion](https://www.notion.so/khk0613/1-d68449d8af1c49b0931cf87a08a29d04)

--- src/js/statement.js
+++ src/js/statement.js
@@ -1,25 +1,26 @@
-
-function amountFor(play, perf) {
+function amountFor(play, aPerformance) {
   let result = 0;
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
   return result;
 }
+
+
 
 export function statement(invoice, plays) {
   let totalAmount = 0;
@@ -31,13 +32,15 @@ export function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(play, perf);
+  function playFor(aPerfomance) {
+    return plays[aPerfomance.playID]
+  }
 
+  for (let perf of invoice.performances) {
+    let thisAmount = amountFor(playFor(perf), perf);
     volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)`;
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)`;
     totalAmount += thisAmount;
   }
   result += `총액: ${format(totalAmount / 100)}`;
