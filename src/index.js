79dfeb424b0d770e101d0cd208381db28ace3692
KHK <ston0538@gmail.com>
2021-06-15 23:38:14 +0900

    fix: 함수를 복사하여 이름 지정

--- src/index.js
+++ src/index.js
@@ -14,3 +14,17 @@ export function alertForMiscreant(people) {
   }
   return "";
 }
+
+export function findMiscreant(people) {
+  for (const p of people) {
+    if (p === "Don") {
+      setOffAlarms();
+      return "Don";
+    }
+    if (p === "John") {
+      setOffAlarms();
+      return "John";
+    }
+  }
+  return "";
+}
