19a4604b6c7344dab61a355614b0470cf27b9876
KHK <ston0538@gmail.com>
2021-08-12 10:46:26 +0900

    feat: 예시 프로그램

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,8 +1,10 @@
-import { Order } from "../index";
+import { statement } from '../../src/index';
+import * as plays from '../../plays.json';
+import * as invoices from '../../invoices.json';
 
-describe("임시 변수를 질의 함수로 바꾸기", () => {
-  it("price", () => {
-    const order = new Order(10, 1000);
-    expect(order.price).toEqual(9500);
+describe('청구서 테스트', () => {
+  test('청구서 출력', () => {
+    const actual = `청구 내역 (고객명: Bigo)\n Hamlet: $650.00 (55석)\n As You Like It: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트: 47점\n`;
+    expect(statement(invoices, plays)).toEqual(actual);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-export class Order {
-  constructor(quantity, item) {
-    this._quantity = quantity;
-    this._item = item;
+export function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
   }
-  get basePrice() {
-    return this._quantity * this._item;
-  }
-  get discountFactor() {
-    let discountFactor = 0.98;
-    if (this.basePrice > 1000) discountFactor -= 0.03;
-    return discountFactor;
-  }
-  get price() {
-    return this.basePrice * this.discountFactor;
-  }
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
 }
