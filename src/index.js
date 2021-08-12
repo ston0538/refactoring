894072ccd690fd47231cada9eab766a3ea481a6a
KHK <ston0538@gmail.com>
2021-08-12 11:30:07 +0900

    fix: play 변수 제거하기

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 export function statement(invoice, plays) {
-  function amountFor(perf, play) {
+  function amountFor(aPerformance, play) {
     let result = 0;
     switch (play.type) {
       case 'tragedy':
         result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case 'comedy':
         result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
-        result += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -29,7 +33,7 @@ export function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
