ad6ce39c87683c13bc9052b3c8ed2914d7a9e517
KHK <ston0538@gmail.com>
2021-08-17 23:27:50 +0900

    refactor: thisAmount 임시 변수를 제거하면서 변수 인라인 하기
    - amountFor() 는 임시 변수인 thisAmount에 값을 설정하는데 사용되는데
    그 값이 다시 바뀌지는 않는다. 따라서 변수 인라인 하기를 적용한다.
    
    - Q. 이렇게 되면 이전에 얘기했던 루프를 돌때마다 공연을 조회하는 계산이
      더 늘어나는걸까?

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ export function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -17,10 +16,10 @@ export function statement(invoice, plays) {
       volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(
+      amountFor(perf, playFor(perf)) / 100
+    )} (${perf.audience}석)\n`;
+    totalAmount += amountFor(perf, playFor(perf));
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
