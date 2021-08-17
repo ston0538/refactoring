6cbce36227bde2cce1970afff619e56dcd18c60d
KHK <ston0538@gmail.com>
2021-08-17 23:10:16 +0900

    refactor: amountFor 함수에서 playFor를 사용하도록 변경
    - playFor와 amountFor 함수를 statement 밖으로 빼냈는데 다시 인라인
      함수로 변경하였다.
    - playFor 함수에 plays와 amountFor 함수에서 playFor 함수를 불러와야 하기
      때문에

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
 export function statement(invoice, plays) {
-  function amountFor(aPerformance, play) {
-    let result = 0;
-    switch (play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -32,6 +7,7 @@ export function statement(invoice, plays) {
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+
   for (let perf of invoice.performances) {
     let thisAmount = amountFor(perf, playFor(perf));
     // add volume credits
@@ -49,4 +25,30 @@ export function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance, play) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
 }
