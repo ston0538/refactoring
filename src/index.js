28a58d03a2a6a85ba1683c0ea823ca1a6ad0cec0
KHK <ston0538@gmail.com>
2021-08-18 00:08:27 +0900

    refactor: 임시 변수에 담은 format 함수를 직접 선언으로 변경

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ export function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
@@ -55,5 +50,12 @@ export function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
+  }
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber);
   }
 }
