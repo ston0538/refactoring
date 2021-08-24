f133468fbb3d309faebc5ad0b1274c9c26a886d6
KHK <ston0538@gmail.com>
2021-08-25 01:03:59 +0900

    refactor: performances(공연 정보)를 중간 데이터 구조로 옮긴다.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 export function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  return renderPlainText(statementData, invoice, plays);
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     result += ` ${playFor(perf).name}: ${usd(
       amountFor(perf, playFor(perf))
     )} (${perf.audience}석)\n`;
@@ -18,14 +19,14 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf, playFor(perf));
     }
     return result;
   }
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
