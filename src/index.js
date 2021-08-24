2c372e3e256a1ef452a39255ba8dd51f7388ab63
KHK <ston0538@gmail.com>
2021-08-24 23:08:53 +0900

    refactor: Slide Statements(문장 슬라이드하기) 적용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(
@@ -10,6 +9,8 @@ export function statement(invoice, plays) {
     )} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf, playFor(perf));
   }
+
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
