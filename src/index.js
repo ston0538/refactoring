c28d99a89aa04ef8420ae8f8059618307e636287
hk <ston0538@gmail.com>
2021-08-31 16:05:33 +0900

    refactor: Split Phase 적용을 위해 Extract Function 적용

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(
@@ -32,13 +35,13 @@ export function statement(invoice, plays) {
   function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
-      case 'tragedy':
+      case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
           result += 1000 * (aPerformance.audience - 30);
         }
         break;
-      case 'comedy':
+      case "comedy":
         result = 30000;
         if (aPerformance.audience > 20) {
           result += 10000 + 500 * (aPerformance.audience - 20);
@@ -55,15 +58,15 @@ export function statement(invoice, plays) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ('comedy' === playFor(aPerformance).type)
+    if ("comedy" === playFor(aPerformance).type)
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
   }
   function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
   }
