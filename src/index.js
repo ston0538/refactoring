42ffa78b8e3b7b79f4e0e38fd73730672f6c90fd
hk <ston0538@gmail.com>
2021-08-31 18:42:21 +0900

    refactor: enrichPerformance 함수를 이용하여 amountFor 함수를 밖으로 이동

--- src/index.js
+++ src/index.js
@@ -7,39 +7,12 @@ export function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = { ...aPerformance };
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-}
-function renderPlainText(data) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
   function amountFor(aPerformance) {
     let result = 0;
     switch (aPerformance.play.type) {
@@ -58,6 +31,31 @@ function renderPlainText(data) {
         break;
       default:
         throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+    return result;
+  }
+}
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += volumeCreditsFor(perf);
     }
     return result;
   }
