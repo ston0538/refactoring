8a09df5b9b60c8c015a3fc0e5b9c5a38c8149924
hk <ston0538@gmail.com>
2021-08-31 19:10:30 +0900

    refactor: refactor: 계산식인 totalVolumeCredits를 statement 함수로 이동

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ export function statement(invoice, plays) {
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+    return result;
+  }
 
   function totalAmount(data) {
     let result = 0;
@@ -20,9 +29,11 @@ export function statement(invoice, plays) {
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
+
   function amountFor(aPerformance) {
     let result = 0;
     switch (aPerformance.play.type) {
@@ -44,6 +55,7 @@ export function statement(invoice, plays) {
     }
     return result;
   }
+
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
@@ -54,23 +66,15 @@ export function statement(invoice, plays) {
     return result;
   }
 }
+
 function renderPlainText(data) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
-
   result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
