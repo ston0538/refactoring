26d45b316dd11fcb3d4ca13eedc55c5887736110
KHK <ston0538@gmail.com>
2021-09-01 00:46:33 +0900

    refactor: volumeCreditsFor 함수를 계산기로 이동

--- createStatementData.js
+++ createStatementData.js
@@ -24,6 +24,15 @@ class PerformanceCalculator {
     }
     return result;
   }
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performances.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ('comedy' === this.play.type)
+      result += Math.floor(this.performances.audience / 5);
+
+    return result;
+  }
 }
 function createStatementData(invoice, plays) {
   const statementData = {};
@@ -60,13 +69,8 @@ function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
 }
 
